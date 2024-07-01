f2277c830e290a7a822e9d41a48cbd18cc467ee0
Soykat Amin <soykatamin@gmail.com>
2024-07-01 16:37:33 +0200

    Gitignore and some little change

--- src/index.php
+++ src/index.php
@@ -7,10 +7,10 @@
 <body>
     <h2>Available Pages</h2>
     <ul>
-        <li><a href="login.php">Login (commento di fine riga)</a></li>
-        <li><a href="search.php">Search User (Tautologia)</a></li>
-        <li><a href="details.php">User Details (Tautologia)</a></li>
-        <li><a href="insert.php">Insert User (Piggybacking)</a></li>
+        <li><a href="login.php">Login (End-of-line comment)</a></li>
+        <li><a href="search.php">Search User (Tautology)</a></li>
+        <li><a href="details.php">User Details (Tautology)</a></li>
+        <li><a href="insert.php">Insert User (Piggybacked queries)</a></li>
         <li><a href="database.php">Database</a></li>
     </ul>
 </body>
